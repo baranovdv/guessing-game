a8dc5b257aaa84661cec6f60dc98e4b6ca4a142c
baranovdv <baranov.d.v.1988@gmail.com>
2023-02-22 23:53:54 +0700

    solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
+
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.left = null;
+        this.right = null;
+        this.value = null;;
+        this.count = function() {
+            this.value = Math.round((this.left + this.right) / 2)
+        }
+    }
+ 
     setRange(min, max) {
-
+       this.left = min
+       this.right = max
     }
 
     guess() {
-
+        this.count();
+        return this.value;
     }
 
     lower() {
-
+        this.right = this.value
     }
 
     greater() {
-
+        this.left = this.value
     }
 }
 
